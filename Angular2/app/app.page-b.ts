676bba8883b8f25d8823c18cc8be7b646a77987d
SaloniSharma2111 <23225047+SaloniSharma2111@users.noreply.github.com>
2018-01-22 15:27:19 -0800

    Angular2 with Typescript

---
+++ Angular2/app/app.page-b.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { ActivatedRoute, Params }       from '@angular/router';
+
+@Component({
+    template: `ID: {{id}}<br/>
+               Name: {{firstName}}`
+})
+export class PageBComponent {
+    private id;
+    private firstName;
+
+    constructor(
+        private route: ActivatedRoute) {
+    }
+
+    ngOnInit() {
+        this.route.params.forEach((params: Params) => {
+            let localID    = params['id'];
+            let localName  = params['firstname'];
+            this.id        = localID;
+            this.firstName = localName;
+        });
+    }
+}
